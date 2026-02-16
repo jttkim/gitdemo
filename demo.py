8d51e0ba99ada99614e787cd08b5e467afa74228
Jan T Kim <jttkim@gmail.com>
2026-02-16 15:28:20 +0000

    fixed ignoring of headers

--- demo.py
+++ demo.py
@@ -4,10 +4,11 @@ v = [[1, 4, 5.5], [2, 6, 6.6]]
 
 d = []
 with open('demo.tsv', 'r') as f :
-    f.readline()
+    hList = f.readline().strip().split()
     l = f.readline()
     while l != '':
         wList = l.strip().split('\t')
         d.append([float(w) for w in wList])
         l = f.readline()
+print(hList)
 print(d)
